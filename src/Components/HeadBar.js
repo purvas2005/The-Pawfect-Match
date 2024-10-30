fad7cdfe535aa9eb236470353615b4c074ec7a42
Purva <spurva449@gmail.com>
2024-10-30 12:34:40 +0530

    Nuked login button

--- src/Components/HeadBar.js
+++ src/Components/HeadBar.js
@@ -10,15 +10,6 @@ const Tab=()=>{
             <img src={Logo} alt="Logo" className="logo" />
             <h1 className="tab-title">Pawfect Match🐾🖤</h1>
             <div className="login-container">
-            <button onClick={() => setShowLogin(!showLogin)}>
-        {showLogin ? 'Close Login' : 'Login'}
-      </button>
-      
-      {/* Login component rendered outside the button */}
-      {showLogin && <Login />}
-
-            </div>
-            <div className="login-container">
                 <button padding>Logout</button>
             </div>
             </div>
